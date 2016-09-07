0da720bce89977051a446fce2dbf9f6d7c7aa4ed
Jim Strother <strotherwebdev@gmail.com>
2016-09-07 03:48:45 +0000

    added bar chart for skillset; added commmented-out addresses for projects

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,21 @@ const velocity = require('velocity-animate');
 
 $(document).ready(function() {
     console.log('ready');
+    
+    // skillset
+    $(document).ready(function() {
+        $('.html').css('width', '100%');
+        $('.css').css('width', '100%');
+        $('.scss').css('width', '95%');
+        $('.bourbon').css('width', '85%');
+        $('.javascript').css('width', '95%');
+        $('.jquery').css('width', '90%');
+        $('.velocity').css('width', '65%');
+        $('.rwd').css('width', '80%');
+        $('.react').css('width', '70%');
+        $('.node').css('width', '100%');
+        $('.socket').css('width', '85%');
+        $('.rethink').css('width', '70%');
+        $('.git').css('width', '95%');
+    });
 });
